c877ef32f7e01a95d1fdcdd9e3febc885cd34c95
TaeheeWoo94 <wth0086@gmail.com>
2023-02-06 14:32:25 +0900

    ssss

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,25 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-BUILD_JAR=$(ls /home/ubuntu/action/build/libs/practice-githubAction-deploy-0.0.1-SNAPSHOT.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-
-echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
-
-echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/
-cp $BUILD_JAR $DEPLOY_PATH
-
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
-
-if [ -z $CURRENT_PID ]
-then
-  echo "> kill -15 $CURRENT_PID" >> /home/ubuntu/action/deploy.log
-  sudo kill -15 $CURRENT_PID
-  sleep 5
-fi
-
-
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포" >> /home/ubuntu/action/deploy.log
-sudo nohub java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_arr.log &
+echo "아무말" >> /home/ubuntu/action/deploy.log
